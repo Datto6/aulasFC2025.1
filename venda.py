5215d748d8ed2c84c783e079eb7cfff0aa37fec4
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-06-13 21:48:30 -0300

    mesmo de antes

--- venda.py
+++ venda.py
@@ -108,3 +108,23 @@ for key,value in dic.items():
 
 print(dic)
 print(dic2)
+
+dic={}
+while True:
+    key=input("Fala o indice. Aperta f se quiser parar")
+    if key=="f":
+        break
+    dic[key]=int(input("Fala o valor q c quer pro indice q vc acabou de botar"))
+
+dic2={}
+print("Agora p segunda dic")
+for key in dic.keys():
+    dic2[key]=int(input("Fala o numero pro indice "+str(key)))
+
+dic3={}
+for key,value in dic.items():
+    dic3[key]=dic[key]+dic2[key]
+
+print(dic)
+print(dic2)
+print(dic3)
