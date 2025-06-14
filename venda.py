4a340f5d35a2526933c0711f51f42602006a2602
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-06-13 21:47:35 -0300

    Congregando tudo aula 06/03/2025

--- venda.py
+++ venda.py
@@ -82,3 +82,29 @@ for key,value in dic.items():
 
 print(dic)
 print(dic2)
+
+# palavra=input("Da uma palavra ae") --> conta chars na palavra
+# dic={}
+# for i in palavra:
+#     cont=0
+#     for j in palavra:
+#         if j==i:
+#             cont+=1
+
+#     dic[i]=cont
+
+# print(dic)
+
+dic={}
+while True:
+    key=input("Fala o indice. Aperta f se quiser parar")
+    if key=="f":
+        break
+    dic[key]=input("Fala o valor q c quer pro indice q vc acabou de botar")
+
+dic2={}
+for key,value in dic.items():
+    dic2[value]=key
+
+print(dic)
+print(dic2)
