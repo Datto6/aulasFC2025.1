e8c23c6951970d275e8bbd636df96ebe352f0e0a
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-06-17 11:02:41 -0300

    +1 aula de funcoes
    16/06/2025

--- 1aulafuncoes.py
+++ 1aulafuncoes.py
@@ -16,3 +16,48 @@ try :
     print(soma(numeros))
 except:
     print("Deu ruim cr")
+
+def criamatriz():
+    matriz=[]
+    linhas=int(input("Quantas linhas tu quer"))
+    colunas=int(input("Quantas colunas"))
+    for i in range(linhas):
+        linha=[]
+        for j in range(colunas):
+            linha.append(input("Fale valor para linha"+str(i)+" Coluna"+str(j)))
+        matriz.append(linha)
+    return matriz
+
+def soma_colunas(matriz):
+    soma=[]
+    for j in range(len(matriz[0])):
+        contador=0
+        for i in range(len(matriz)):
+            contador+=int(matriz[i][j])
+        soma.append(contador)
+    return soma
+
+def palindromao(frases):
+    saida=[]
+    for i in frases:
+        stringuin=i.split()
+        for i in stringuin:
+            ajustado=i.lower()
+            if ajustado[::-1]==ajustado and ajustado.isalpha():
+                saida.append(i)
+
+    return saida
+def listastring():
+    saida=[]
+    numstring=int(input("Quantos strings vc quer"))
+    for i in range(numstring):
+        saida.append(input("Da 1 ai"))
+    return saida
+
+
+def rotacionar(lista,numero):
+    for i in range(numero):
+        lista.insert(0,lista.pop())
+    return lista
+
+print(rotacionar([1,2,3,4],3))
